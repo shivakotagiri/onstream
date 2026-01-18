e04ed15b65afc038cb9f52ef561492daceb48fd6
shivaKotagiri <shivakotagiri532@gmail.com>
2026-01-18 23:59:56 +0530

    small changes

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Login() {
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,2 +1,8 @@
 import { createAuthClient } from "better-auth/client";
 export const authClient = createAuthClient();
+
+export const signIn = async () => {
+    await authClient.signIn.social({
+        provider: 'google'
+    });
+}

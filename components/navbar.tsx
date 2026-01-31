32e3ca02bda3ac243c5e8a298569eb5bf46b72a3
shivaKotagiri <shivakotagiri532@gmail.com>
2026-01-31 21:17:01 +0530

    changed the theme colors

---
+++ components/navbar.tsx
@@ -0,0 +1,7 @@
+export function Navbar() {
+    return (
+        <div className="h-16 fixed border  border-white w-full flex items-center justify-between">
+            Navbar!!
+        </div>
+    )
+}

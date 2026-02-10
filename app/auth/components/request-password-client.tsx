3f1bf80420fb05b0efcba98fe77743a9ce877437
shivaKotagiri <shivakotagiri532@gmail.com>
2026-02-10 19:43:21 +0530

    fixing reset password build issues

---
+++ app/auth/components/request-password-client.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { RequestResetPasswordEmail } from "../components/request-password-reset-email";
+import { PasswordReset } from "../components/password-reset";
+import { useSession } from "@/lib/auth-client";
+
+
+export function ResetPasswordClient() {
+    const searchParams = useSearchParams();
+    const token = searchParams.get("token");
+
+    const router = useRouter();
+
+    const session = useSession();
+    
+    useEffect(() => {
+        if(session.data) {
+            router.replace("/")
+        }
+    },[session, router]);
+    
+    if(session?.data) return null;
+
+    return (
+        <div className="h-screen w-screen flex justify-center items-center">
+            {!token && <RequestResetPasswordEmail/>}
+            {token && <PasswordReset token={token} />}
+        </div>
+    )
+}
--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,34 +1,7 @@
-"use client";
-
-export const dynamic = "force-dynamic";
-
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { RequestResetPasswordEmail } from "../components/request-password-reset-email";
-import { PasswordReset } from "../components/password-reset";
-import { useSession } from "@/lib/auth-client";
-
+import { ResetPasswordClient } from "../components/request-password-client";
 
 export default function ResetPassword() {
-    const searchParams = useSearchParams();
-    const token = searchParams.get("token");
-
-    const router = useRouter();
-
-    const session = useSession();
-    
-    useEffect(() => {
-        if(session.data) {
-            router.replace("/")
-        }
-    },[session, router]);
-    
-    if(session?.data) return null;
-
     return (
-        <div className="h-screen w-screen flex justify-center items-center">
-            {!token && <RequestResetPasswordEmail/>}
-            {token && <PasswordReset token={token} />}
-        </div>
+        <ResetPasswordClient />
     )
 }

2a2c989bf47e6e7acf56244a45d71a375da4948d
shivaKotagiri <shivakotagiri532@gmail.com>
2026-04-02 11:43:11 +0530

    fix: removed the getStreamById function from the chat page

--- actions/stream.ts
+++ actions/stream.ts
@@ -3,6 +3,9 @@ import { user } from "@/db/schema"
 import { eq } from "drizzle-orm"
 
 export const getStreamByUserId = async (userId: string) => {
+    if(!userId) {
+        return null
+    }
     const stream = await db.query.stream.findFirst({
         where: eq(user.id, userId),
     });
--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,12 +1,10 @@
-import { getStreamByUserId } from "@/actions/stream"
 import { getInfo } from "@/lib/get-session";
 import { redirect } from "next/navigation";
 
 export default async function ChatPage() {
     const data = await getInfo();
-    const currentUser = data?.currentUser;
+    const currentUser = data?.currentUser || null;
     if(!currentUser) redirect("/");
-    const streamData = await getStreamByUserId(currentUser?.id || "");
     return (
         <div>
             Chat Page

9691ef8bddf39228e5f8580a26fc8f65b3879710
shivaKotagiri <shivakotagiri532@gmail.com>
2026-04-12 18:44:51 +0530

    added the video component

--- app/(dashboard)/u/[username]/(home)/_components/stream-player.tsx
+++ app/(dashboard)/u/[username]/(home)/_components/stream-player.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { User, Stream } from "@/db/schema"
@@ -15,7 +16,7 @@ export function StreamPlayer({ user, stream, isFollowing }: StreamPlayerProps) {
     const { token, name, identity } = useViewerToken(user.id);
 
     if(!token || !name || !identity) {
-        return <div>
+        return <div className="flex justify-center items-center w-full h-[90vh]">
             Cannot watch the stream {token} ------ {name} ---- {identity}
         </div>
     }
@@ -26,6 +27,10 @@ export function StreamPlayer({ user, stream, isFollowing }: StreamPlayerProps) {
                 className="grid grid-cols-1 lg:gap-y-0 xl:grid-cols-3 2xl:grid-cols-6 h-full"
                 serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL!} 
                 token={token}
+                options={{
+                    dynacast: true,
+                    adaptiveStream: true
+                }}
             >
                 <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
                     <Video 

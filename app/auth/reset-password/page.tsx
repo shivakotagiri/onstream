d9e9e71801d0e7d96c2814b1f8ecf3b413ff59c4
shivaKotagiri <shivakotagiri532@gmail.com>
2026-06-04 00:37:17 +0530

    added a suspense reset-password page

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ResetPasswordClient from "../_components/request-password-client";
 
 export default function Page() {
-  return <ResetPasswordClient />;
+  return (
+    <Suspense fallback={<div className="h-screen w-screen flex items-center justify-center" />}>
+      <ResetPasswordClient />
+    </Suspense>
+  );
 }
